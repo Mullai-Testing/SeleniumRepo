231879ab996574f7b0d31c130384bbb314799032
Mullai-Testing <mullaimalar.tpt@gmail.com>
2025-01-20 18:50:51 +0530

    Add files via upload
    Week1_Day1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("chrome");
+float version =91.0f;
+System.out.println("version: " + version);
+String developer ="google";
+System.out.println("developer: " + developer);
+boolean isBeta = false;
+System.out.println("is Beta: " + isBeta);
+int releaseYear = 2008;
+System.out.println("release Year: " + releaseYear);
+char shortcutKey ='c';
+System.out.println("shortcut Key: " + shortcutKey);
+	}
+
+}
